716998a2a693b95835761d6baea787464a1b29e8
Andrew Och <andrewoch95@gmail.com>
2018-09-27 11:42:54 -0400

    Started to code currPrice

--- my-project-client/src/Stock.js
+++ my-project-client/src/Stock.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const Stock = (props) => {
-  return(
-    <tr>
-      <td>{props.data["Name"]}</td>
-      <td>{props.data["Symbol"]}</td>
-    </tr>
-  )
+
+export default class Stock extends Component{
+  state = {
+    currPrice: 0
+  }
+
+  componentDidMount(){
+    let URL = `https://api.iextrading.com/1.0/stock/${this.props.data["Symbol"]}/book`
+    fetch(URL).then(
+      res => res.json()).then(data =>
+        this.setState({
+          currPrice: data.quote.latestPrice
+        })
+      )
+  }
+
+  render(){
+    return(
+      <tr>
+        <td>{this.props.data["Name"]}</td>
+        <td>{this.props.data["Symbol"]}</td>
+        <td>{this.state.currPrice}</td>
+      </tr>
+    )
+  }
 
 }
-
-
-export default Stock
